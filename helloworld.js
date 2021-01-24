0398291c73324bd85529010ec286ba6bc7b0ca5e
jalongi11 <76964546+jalongi11@users.noreply.github.com>
2021-01-24 13:53:24 -0600

    Add files via upload

---
+++ helloworld.js
@@ -0,0 +1,13 @@
+import * as Tone from 'tone'
+
+const synth = new Tone.Synth().toMaster()
+synth.triggerAttackRelease('C4', '8n')
+
+
+document.getElementById("play-button").addEventListener("click", function() {
+  if (Tone.Transport.state !== 'started') {
+    Tone.Transport.start();
+  } else {
+    Tone.Transport.stop();
+  }
+});
